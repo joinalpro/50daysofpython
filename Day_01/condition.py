668740940a78fec0af56e35044820537103bbb25
joinalzx <joinalzx@gmail.com>
2025-03-04 14:12:30 +0600

    conditional statement

---
+++ Day_01/condition.py
@@ -0,0 +1,20 @@
+# Sequencial Statements
+# Control flow / Conditional Statements(depend on condition
+# Repeatable Statements
+
+# Use indentation
+
+# x = 10
+# if x > 5:
+#     print("X is greater than 5")
+# else:
+#     print("X is less than 5")
+    
+is_rain_tomorrow = True
+if not is_rain_tomorrow:
+    print("Go to outside")
+    print("working on field")
+else:
+    print("I will do indoor work for two hours.")
+    
+    
