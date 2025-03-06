d9b630f97d5f33f06c9185053e7c559fcb151ef0
Joinal Abden <joinalzx@gmail.com>
2025-03-06 23:58:59 +0600

    for loop

--- day_03/loop.py
+++ day_03/loop.py
@@ -1,1 +1,9 @@
 # for loop
+# 3 types of statement i, sequencial ii, conditional iii, repeatable statement
+
+# Iterables: list , tuple, set, dictionary, strings
+
+nums = [23, 25, 33, 26, 39]
+for item in nums:
+    print("Item ", item ** 10)
+    
