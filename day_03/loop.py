db7f946bb5aa2f4061ff0906736e13fbff201612
Joinal Abden <joinalzx@gmail.com>
2025-03-07 23:31:24 +0600

    loop

--- day_03/loop.py
+++ day_03/loop.py
@@ -4,6 +4,10 @@
 # Iterables: list , tuple, set, dictionary, strings
 
 nums = [23, 25, 33, 26, 39]
+# here item like a variable
+# tuple (2, 3, 4, 5)
+# set {3, 4, 5, 6}
+# dictionary {"key": "value", "name": "joinal"}
 for item in nums:
     print("Item ", item ** 10)
     
