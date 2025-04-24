80b49485a6165973450046fea2ddebd5188b260c
joinalzx <joinalzx@gmail.com>
2025-04-24 20:39:51 +0600

    chapter 2 variables, comments

---
+++ day_04/favorite.py
@@ -0,0 +1,7 @@
+# adding two numbers below and storing this in a new variable then printing that variable.
+firstNumber = 5
+secondNumber = 3
+favNumber = firstNumber + secondNumber
+print(favNumber)
+
+# Hello Everyone, I am reading Python Crash Course book by Eric
