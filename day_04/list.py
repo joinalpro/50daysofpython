cd6c4419f636f54b7344f7df3eae9931f2826e9f
Joinal Abden <joinalzx@gmail.com>
2025-05-08 00:13:23 +0600

    list

--- day_04/list.py
+++ day_04/list.py
@@ -1,6 +1,13 @@
-squares = []
-for num in range(2, 11):
-     square = num ** 2
-     squares.append(square)
-print(squares)
+# squares = []
+# for num in range(2, 11):
+#      square = num ** 2
+#      squares.append(square)
+# print(squares)
 
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
